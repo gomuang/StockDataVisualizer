812951476f4feb693f4c3ebd1407f4e455abdf2d
Lenore Riley <rileyle@umsystem.edu>
2025-03-17 18:33:00 -0500

    add comments for organization

--- stocks.py
+++ stocks.py
@@ -7,3 +7,11 @@ import web-browser
 
 API_KEY = "L0WWWUGT9503R130"
 BASE_URL = "https://www.alphavantage.co/query"
+
+#get company symbol from user
+
+#get chart type user wants displayed
+
+#get time series choice from user
+
+#get start and end date for chart render
